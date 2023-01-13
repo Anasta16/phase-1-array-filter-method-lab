45a5cdbe135b2082c161cf9e24f68d813d4cea10
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2023-01-13 16:23:31 -0500

    commit tree a01523bb4ca9f382c0e3a412b23716ee17663ef2

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+
+// const drivers = ['Bobby', "Sammy", "Sally", "Annette", " Sarah", "Bobby", "bobby"]
+
+const drivers = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh'
+    }, 
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    }
+]
+
+function findMatching(drivers, name) {
+    return drivers.filter(
+        (driver) => driver.toLowerCase() === name.toLowerCase()
+        )
+}
+
+// console.log(findMatching(drivers, 'bobby'))
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter((driver) =>
+     driver.startsWith(letters))
+     
+ }
+
+ function matchName(drivers, name) {
+    return drivers.filter((driver) => {
+        return driver.name === name
+    }
+)
+}
